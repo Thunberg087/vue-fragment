f0e671c54b377d0daf694631681a68ea529b1117
Julien Barbay <julien.barbay@linecorp.com>
2018-06-14 13:40:03 +0900

    removed functional impl bc/ it doesnt support local directives (yet)

--- src/component.js
+++ src/component.js
@@ -1,14 +1,25 @@
 import directive from './directive'
 
+/*
+// wait for PR 'assets for functional components' to pass
 export default {
   functional: true,
+  directives: {
+    'fragment': directive
+  },
   render: function(h, context) {
     return h(
       'div', {
         attrs: { class: 'fragment' },
-        directives: { fragment: directive }
+        directives: [{ name: 'fragment' }]
       },
       [context.children]
     )
   }
+}
+*/
+
+export default {
+  directives: { 'fragment': directive },
+  template: `<div class='v-fragment' v-fragment><slot /></div>`
 }
