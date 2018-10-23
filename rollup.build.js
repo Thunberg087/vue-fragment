ba35fa1a6609258b02c32825fe98aaf3580a28dc
Julien Barbay <barbay.julien@gmail.com>
2018-10-23 17:56:41 +0900

    transpile before build

--- rollup.build.js
+++ rollup.build.js
@@ -1,10 +1,20 @@
 const rollup = require('rollup'),
       minify = require('rollup-plugin-minify-es')
+      babel  = require('rollup-plugin-babel')
 
-const i = { input: './src/index.js', plugins: [minify()] },
-      o = { file: './dist/vue-fragments.min.js',
+const i = {
+            input: './src/index.js',
+            plugins: [
+              babel({ exclude: 'node_modules/**' }),
+              minify()
+            ]
+          },
+
+      o = {
+            file: './dist/vue-fragments.min.js',
             format: 'umd',
-            name: 'VFragments'
+            name: 'Fragment',
+            exports: 'named',
           }
 
 async function build() {
