57eaa1239dcb897862a38be533c61060879a3677
Julien Barbay <barbay.julien@gmail.com>
2018-10-24 12:34:26 +0900

    esm module build

--- rollup.build.js
+++ rollup.build.js
@@ -4,26 +4,33 @@ const rollup = require('rollup'),
       minify = require('rollup-plugin-minify-es'),
       babel  = require('rollup-plugin-babel')
 
-const i = {
-            input: './src/index.js',
-            plugins: [
-              babel({ exclude: 'node_modules/**' }),
-              minify()
-            ]
-          },
+const source = {
+  input: './src/index.js',
+  plugins: [
+    babel({ exclude: 'node_modules/**' }),
+    minify()
+  ]
+}
 
-      o = {
-            file: config.main,
-            format: 'umd',
-            name: config.global,
-            exports: 'named',
-          }
+const umd = {
+  file: config.main,
+  format: 'umd',
+  name: config.global,
+  exports: 'named',
+}
+
+const esm = {
+  file: config.module,
+  format: 'esm',
+}
 
 async function build() {
-  const bundle = await rollup.rollup(i)
-  const { code, map } = await bundle.generate(o)
+  const bundle = await rollup.rollup(source)
 
-  await bundle.write(o)
+  ;[umd, esm].forEach(build => {
+    bundle.generate(build)
+    bundle.write(build)
+  })
 }
 
 build()
